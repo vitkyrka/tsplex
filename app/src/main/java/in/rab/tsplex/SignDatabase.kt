68a0e5e5a513a9c203bf5a96e3273fbf8633b797
Rabin Vincent <rabin@rab.in>
2018-02-25 15:01:02 +0100

    Update database

--- app/src/main/java/in/rab/tsplex/SignDatabase.kt
+++ app/src/main/java/in/rab/tsplex/SignDatabase.kt
@@ -206,7 +206,7 @@ class SignDatabase(context: Context) {
 
     companion object {
         private val DATABASE_NAME = "signs.jet"
-        private val DATABASE_VERSION = 26
+        private val DATABASE_VERSION = 27
 
         private fun buildColumnMap(): HashMap<String, String> {
             val map = HashMap<String, String>()
