e39f667795999070f77ba276f91240616620737f
Rabin Vincent <rabin@rab.in>
2018-02-28 13:33:20 +0100

    Remove unused imports

--- app/src/main/java/in/rab/tsplex/LocalListFragment.kt
+++ app/src/main/java/in/rab/tsplex/LocalListFragment.kt
@@ -1,8 +1,5 @@
 package `in`.rab.tsplex
 
-import android.database.Cursor
-import android.database.sqlite.SQLiteDatabase
-
 abstract class LocalListFragment(val idTable: String, val orderBy: String) : SignListFragment() {
     override fun getSigns(): ArrayList<Sign> {
         return SignDatabase(activity).getSignsByIds(idTable, orderBy)
