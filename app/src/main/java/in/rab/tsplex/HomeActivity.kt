20624ab40ab6002d01e6bfabd57dbb20ef0cc376
Vincent Whitchurch <vincent@whitchurch.se>
2019-01-12 19:35:23 +0100

    Restore title in home

--- app/src/main/java/in/rab/tsplex/HomeActivity.kt
+++ app/src/main/java/in/rab/tsplex/HomeActivity.kt
@@ -5,6 +5,7 @@ import android.app.SearchManager
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
+import android.support.design.widget.NavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
@@ -12,9 +13,11 @@ import android.view.Menu
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_home.*
 
-class HomeActivity : AppCompatActivity(), SignListFragment.OnListFragmentInteractionListener, TopicListFragment.OnTopicClickListener {
+class HomeActivity : AppCompatActivity(), SignListFragment.OnListFragmentInteractionListener, TopicListFragment.OnTopicClickListener, NavigationView.OnNavigationItemSelectedListener {
     private var mOrdboken: Ordboken? = null
     private var mDrawerToggle: ActionBarDrawerToggle? = null
+    private var mActionBar: android.support.v7.app.ActionBar? = null
+    private var mTitle: String? = null
 
     override fun onListFragmentInteraction(item: Sign) {
         val intent = Intent(this, SignActivity::class.java)
@@ -30,6 +33,12 @@ class HomeActivity : AppCompatActivity(), SignListFragment.OnListFragmentInterac
         startActivity(intent)
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("title", mTitle)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -42,51 +51,61 @@ class HomeActivity : AppCompatActivity(), SignListFragment.OnListFragmentInterac
                 R.string.drawer_close)
         drawer_layout.addDrawerListener(mDrawerToggle!!)
 
-        val actionBar = supportActionBar ?: return
-        actionBar.setDisplayHomeAsUpEnabled(true)
-        actionBar.setHomeButtonEnabled(true)
+        mActionBar = supportActionBar ?: return
+        mActionBar!!.setDisplayHomeAsUpEnabled(true)
+        mActionBar!!.setHomeButtonEnabled(true)
 
-        navigation.setNavigationItemSelectedListener {
-            it.isChecked = true
-            drawer_layout.closeDrawers()
+        navigation.setNavigationItemSelectedListener(this)
 
-            when (it.itemId) {
-                R.id.navigation_history -> {
-                    val fragment: Fragment? = HistoryFragment.newInstance()
-                    supportFragmentManager.beginTransaction().replace(R.id.content, fragment!!).commit()
-                    actionBar.setTitle(R.string.title_history)
-                    return@setNavigationItemSelectedListener true
-                }
-                R.id.navigation_favorites -> {
-                    val fragment: Fragment? = FavoritesFragment.newInstance()
-                    supportFragmentManager.beginTransaction().replace(R.id.content, fragment!!).commit()
-                    actionBar.setTitle(R.string.title_favorites)
-                    return@setNavigationItemSelectedListener true
-                }
-                R.id.navigation_topics -> {
-                    val fragment: Fragment? = TopicListFragment.newInstance()
-                    supportFragmentManager.beginTransaction().replace(R.id.content, fragment!!).commit()
-                    actionBar.setTitle(R.string.title_topics)
-                    return@setNavigationItemSelectedListener true
-                }
-                R.id.navigation_examples -> {
-                    val fragment: Fragment? = ExampleListFragment.newInstance()
-                    supportFragmentManager.beginTransaction().replace(R.id.content, fragment!!).commit()
-                    actionBar.setTitle(R.string.title_examples)
-                    return@setNavigationItemSelectedListener true
-                }
-                else -> {
-                    return@setNavigationItemSelectedListener false
-                }
+        if (savedInstanceState != null) {
+            mTitle = savedInstanceState.getString("title")
+        }
+
+        if (mTitle == null) {
+            onNavigationItemSelected(navigation.menu.findItem(R.id.navigation_history))
+        } else {
+            mActionBar!!.setTitle(mTitle)
+        }
+    }
+
+    private fun setAndSaveTitle(res: Int) {
+        mTitle = getString(res)
+        mActionBar!!.setTitle(res)
+    }
+
+    override fun onNavigationItemSelected(it: MenuItem): Boolean {
+        it.isChecked = true
+        drawer_layout.closeDrawers()
+
+        when (it.itemId) {
+            R.id.navigation_history -> {
+                val fragment: Fragment? = HistoryFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.content, fragment!!).commit()
+                setAndSaveTitle(R.string.title_history)
+                return true
+            }
+            R.id.navigation_favorites -> {
+                val fragment: Fragment? = FavoritesFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.content, fragment!!).commit()
+                setAndSaveTitle(R.string.title_favorites)
+                return true
+            }
+            R.id.navigation_topics -> {
+                val fragment: Fragment? = TopicListFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.content, fragment!!).commit()
+                setAndSaveTitle(R.string.title_topics)
+                return true
+            }
+            R.id.navigation_examples -> {
+                val fragment: Fragment? = ExampleListFragment.newInstance()
+                supportFragmentManager.beginTransaction().replace(R.id.content, fragment!!).commit()
+                setAndSaveTitle(R.string.title_examples)
+                return true
+            }
+            else -> {
+                return false
             }
         }
-
-        val fragment: Fragment? = HistoryFragment.newInstance()
-        if (fragment != null) {
-            supportFragmentManager.beginTransaction().replace(R.id.content, fragment).commit()
-        }
-        actionBar.setTitle(R.string.title_history)
-        navigation.menu.findItem(R.id.navigation_history).isChecked = true
     }
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
