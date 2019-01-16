4a659a706a69c18fc24242a081c9aaf2af9d4a4b
Vincent Whitchurch <vincent@whitchurch.se>
2019-01-16 21:00:53 +0100

    Play preview on transcription click

--- app/src/main/java/in/rab/tsplex/SignRecyclerViewAdapter.kt
+++ app/src/main/java/in/rab/tsplex/SignRecyclerViewAdapter.kt
@@ -1,16 +1,18 @@
 package `in`.rab.tsplex
 
+import android.annotation.SuppressLint
+import android.opengl.Visibility
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.ViewFlipper
+import android.widget.*
 import com.bumptech.glide.Priority
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.request.RequestOptions
+import kotlin.math.min
 
 class SignRecyclerViewAdapter(private val mSigns: List<Sign>,
                               private val mExamples: List<Example>,
@@ -28,6 +30,7 @@ class SignRecyclerViewAdapter(private val mSigns: List<Sign>,
         return ViewHolder(view, viewType)
     }
 
+    @SuppressLint("SetTextI18n")
     private fun bindSign(holder: ViewHolder, position: Int) {
         val sign = mSigns[position]
         holder.mItem = sign
@@ -35,12 +38,38 @@ class SignRecyclerViewAdapter(private val mSigns: List<Sign>,
 
         val urls = sign.getImageUrls()
 
+        holder.mImages.stopFlipping()
+        holder.mImages.displayedChild = 0
+
+        val realCount = sign.transcription.codePointCount(0, sign.transcription.length)
+        val numShow = 20
+
+        if (realCount > numShow) {
+            val cutOffset = sign.transcription.offsetByCodePoints(0, min(numShow, realCount))
+            holder.mTranscriptionText.text = sign.transcription.substring(0, cutOffset) + "…"
+        } else {
+            holder.mTranscriptionText.text = sign.transcription
+        }
+
         for (view in holder.imageViews) {
             view.layoutParams = mLayoutParams
         }
 
         val lowOptions = RequestOptions.priorityOf(Priority.LOW)
         val highOptions = RequestOptions.priorityOf(Priority.HIGH)
+
+        if (urls.size > 1) {
+            holder.mTranscriptionText.setOnClickListener {
+                if (holder.mImages.isFlipping) {
+                    holder.mImages.stopFlipping()
+                    holder.mImages.displayedChild = 0
+                } else {
+                    holder.mImages.startFlipping()
+                }
+            }
+        } else {
+            holder.mTranscriptionText.setOnClickListener(null)
+        }
 
         for ((i, url) in urls.withIndex()) {
             if (i >= holder.imageViews.size) {
@@ -105,6 +134,7 @@ class SignRecyclerViewAdapter(private val mSigns: List<Sign>,
     inner class ViewHolder(val mView: View, val mViewType: Int) : RecyclerView.ViewHolder(mView) {
         val mIdView: TextView = mView.findViewById(R.id.id)
         val mImages: ViewFlipper = mView.findViewById(R.id.images)
+        val mTranscriptionText: TextView = mView.findViewById(R.id.transcriptionText)
         private val imageViewIds = intArrayOf(R.id.image1, R.id.image2, R.id.image3, R.id.image4)
         val imageViews: Array<ImageView> = Array(imageViewIds.size) { i ->
             mView.findViewById<ImageView>(imageViewIds[i])
