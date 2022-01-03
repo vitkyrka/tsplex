39d79578bbcd54f3c35fb951c30c5967be2b3112
Vincent Whitchurch <vincent@whitchurch.se>
2022-01-03 17:54:19 +0100

    Remove requests_cache
    We already needed to work around stuff in request_cache that don't work
    as we want it.  It also seems to open and close the database on every
    request and write to the database even on every cached request making
    things very slow.  Just remove it and add a custom cache.

--- tools/crawl.py
+++ tools/crawl.py
@@ -5,6 +5,8 @@ import os
 import time
 import random
 import logging
+import sqlite3
+import re
 
 from urllib.parse import urlparse
 from pathlib import Path
@@ -12,33 +14,63 @@ from pathlib import Path
 import lxml.html
 
 import requests
-import requests_cache
 
-base = 'http://teckensprakslexikon.su.se'
+base = 'https://teckensprakslexikon.su.se'
 
 
 def make_throttle_hook():
     def hook(response, *args, **kwargs):
-        if not getattr(response, 'from_cache', False):
-            delay = random.uniform(0.0, 1.0)
-            time.sleep(delay)
+        delay = random.uniform(0.0, 1.0)
+        time.sleep(delay)
         return response
     return hook
 
 
-def cache_has_url(s, url):
-    # s.cache.has_url() doesn't actual work for what we want to do.  For
-    # error responses such as 404 the url is in the urls table but there is
-    # no response
-    key = s.cache._url_to_key(url)
+class CachedSession:
+    def __init__(self, session, db, offline):
+        self.session = session
+        self.conn = sqlite3.connect(db)
+        self.offline = offline
 
-    try:
-        respkey = s.cache.keys_map[key]
-        s.cache.responses[respkey]
-        return True
-    except KeyError:
-        return False
+        self.conn.execute("CREATE TABLE IF NOT EXISTS cache (url TEXT, content TEXT, UNIQUE (url) ON CONFLICT REPLACE)")
+        self.conn.execute("CREATE INDEX IF NOT EXISTS cache_index ON cache(url)")
+        self.conn.commit()
+        self.conn.execute("BEGIN")
 
+    def get_from_cache(self, url):
+        cur = self.conn.cursor()
+        # To reuse cache from before the switch to HTTPS
+        url = url.replace("https://", "http://")
+        cur.execute("select content from cache where url = ?", (url,))
+        row = cur.fetchone()
+        cur.close()
+        if not row:
+            return None
+        return row[0]
+
+    def _put_into_cache(self, url, content):
+        # See above
+        url = url.replace("https://", "http://")
+        self.conn.execute("INSERT INTO cache VALUES (?, ?)", (url, content))
+
+    def get(self, url, cached_ok=True):
+        content = None
+        if cached_ok:
+            content = self.get_from_cache(url)
+        if content is None:
+            if self.offline:
+                raise Exception(f"{url} not in cache while in offline mode")
+            resp = self.session.get(url)
+            if resp.ok:
+                content = resp.text
+            elif resp.status_code == 404:
+                content = ''
+            else:
+                resp.raise_for_success()
+
+            self._put_into_cache(url, content)
+
+        return content
 
 def main():
     parser = argparse.ArgumentParser()
@@ -47,26 +79,25 @@ def main():
     parser.add_argument('--overwrite', action='store_true')
     parser.add_argument('--tail', action='store_true')
     parser.add_argument('--offline', action='store_true')
+    parser.add_argument('--db', default='tsplex.sqlite')
     args = parser.parse_args()
 
     logging.basicConfig(level=logging.DEBUG)
-    requests_cache.install_cache('cache')
 
-    s = requests_cache.CachedSession(allowable_codes=(200,404))
-    s.hooks = {'response': make_throttle_hook()}
+    session = requests.Session()
+    session.hooks = {'response': make_throttle_hook()}
+
+    cache = CachedSession(session, args.db, offline=args.offline)
 
     last = args.last
     if not last:
         url = f'{base}/?sortby=fm_id&order=desc'
-        if args.offline:
-            resp = s.get(url)
-        else:
-            s.cache.delete_url(url)
-            resp = s.get(url)
-
-        page = lxml.html.fromstring(resp.text)
+        text = cache.get(url, cached_ok=args.offline)
+        page = lxml.html.fromstring(text)
         lastlink = sorted(page.xpath('//a[contains(@href, "/ord/")]/@href'))[-1]
-        last = int(lastlink.split('/')[-1])
+        last = int(lastlink.split('/')[-1].split('?')[0])
+
+    logging.debug(f"last: {last}")
 
     basepath = Path(urlparse(base).hostname) / 'ord'
 
@@ -78,48 +109,56 @@ def main():
 
     openflags = 'w' if args.overwrite else 'x'
 
-    for i in range(last, 0, -1):
-        signid = f'{i:05d}'
-        signurl = f'{base}/ord/{signid}'
+    try:
+        for i in range(last, 0, -1):
+            signid = f'{i:05d}'
+            signurl = f'{base}/ord/{signid}'
 
-        if args.tail and cache_has_url(s, signurl):
-            break
+            if args.tail and cache.get_from_cache(signurl) is not None:
+                logging.debug(f"{signurl} in cache, stopping --tail run")
+                break
 
-        if args.offline and not cache_has_url(s, signurl):
-            continue
+            if args.offline and cache.get_from_cache(signurl) is None:
+                continue
 
-        resp = s.get(signurl)
-        if not resp.ok:
-            continue
+            page = cache.get(signurl)
+            if not page:
+                continue
 
-        page = resp.text
-        html = lxml.html.fromstring(page)
+            html = lxml.html.fromstring(page)
 
-        if ((i % 1000) == 0):
-            logging.debug(f'Processing {signid}')
-
-        if args.write:
-            try:
-                with open(basepath / f'{signid}.html', openflags) as f:
-                    f.write(page)
-            except FileExistsError:
-                pass
-
-        for url in [url for url in html.xpath('//a/@href') if f'/ord/{signid}' in url and '?' not in url]:
-            what = url.split('/')[-1]
-            page = s.get(base + url).text
+            if ((i % 1000) == 0):
+                logging.debug(f'Processing {signid}')
 
             if args.write:
                 try:
-                    os.makedirs(basepath / signid)
+                    fn = basepath / f'{signid}.html'
+                    with open(fn, openflags) as f:
+                        f.write(page)
+                    logging.debug(f'Wrote {fn}')
                 except FileExistsError:
                     pass
 
-                try:
-                    with open(basepath / signid / f'{what}.html', openflags) as f:
-                        f.write(page)
-                except FileExistsError:
-                    pass
+            for url in [url for url in html.xpath('//a/@href') if f'/ord/{signid}' in url and '?' not in url]:
+                what = url.split('/')[-1]
+                page = cache.get(base + url)
+
+                if args.write:
+                    try:
+                        os.makedirs(basepath / signid)
+                    except FileExistsError:
+                        pass
+
+                    try:
+                        fn = basepath / signid / f'{what}.html'
+                        with open(fn, openflags) as f:
+                            f.write(page)
+                        logging.debug(f'Wrote {fn}')
+                    except FileExistsError:
+                        pass
+    finally:
+        cache.conn.commit()
+        cache.conn.close()
 
 if __name__ == '__main__':
     main()
